425e7cec1ef9fcc41a1a2b075aa80672fb2a7bcf
amikshas <46197570+amikshas@users.noreply.github.com>
2021-12-29 07:56:04 +0530

    Create variables.tf

---
+++ infrastructure/kubernetes/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+description = "access_key for aws"
+type        = string
+default     = "<aws access key>"
+}
+
+variable "secret_key" {
+description = "secret_key for aws"
+type        = string
+default     = "<aws secret key>"
+}
