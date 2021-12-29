271232fc8b7049f0d7ff1c978e3c92c2d85c489a
amikshas <46197570+amikshas@users.noreply.github.com>
2021-12-29 10:12:42 +0530

    Create variables.tf

---
+++ infrastructure/Ansible/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+description = "access_key for aws"
+type        = string
+default     = "<aws_access_key_value>"
+}
+
+variable "secret_key" {
+description = "secret_key for aws"
+type        = string
+default     = "<aws_secret_key_value>"
+}
